0cc329d0892758364c9801ed104b984c64677bab
fkarani <maina.felix@eclectics.io>
2024-01-06 23:43:31 +0300

    first commit

---
+++ src/test/java/com/kafkatutorial/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkatutorial.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
